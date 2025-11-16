1709523bfcea6dee6c30272e2d01e26722eafd87
ali <Ali.Abourizk@alumnos.upm.es>
2025-11-16 22:49:07 +0100

    BoradUpdateShots completado

--- src/main/Test/Board/BoardUpdateShots.java
+++ src/main/Test/Board/BoardUpdateShots.java
@@ -11,69 +11,94 @@ import space_invaders.sprites.Shot;
 import java.util.ArrayList;
 import java.util.List;
 
+import  static org.junit.jupiter.api.Assertions.*;
+
 public class BoardUpdateShots {
+    private Board board;
     @BeforeEach
     public void setup() {
+        board = new Board();
     }
+
     @AfterEach
     public void tearDown() {
+    }
+
+    private void callUpdateOnce() {
+        try {
+            var updateMethod = Board.class.getDeclaredMethod("update");
+            updateMethod.setAccessible(true);
+            updateMethod.invoke(board);
+        } catch (Exception e) {}
     }
 
     //Camino 1-14
     @Test
     public void UpdateShots1(){
-        Board board = new Board();
         board.setShot(new Shot(10,10));
         board.getShot().die();
-        //board.update();
+
+        callUpdateOnce();
+
+        assertFalse(board.getShot().isVisible());
     }
 
-    //Camino 1-2-3-11-1w-14
+    //Camino 1-2-3-11-12-14
     @Test
     public void UpdateShots2(){
-        Board board = new Board();
         board.setShot(new Shot(10,10));
         List<Alien> aliens = new ArrayList<>();
         board.setAliens(aliens);
         board.getShot().setY(-10);
-        //board.update();
+
+        callUpdateOnce();
+
+        assertFalse(board.getShot().isVisible());
     }
     //Camino 1-2-3-11-13-14
     @Test
     public void UpdateShots3(){
-        Board board = new Board();
         board.setShot(new Shot(10,10));
         List<Alien> aliens = new ArrayList<>();
         board.setAliens(aliens);
-        //board.update();
+
+        callUpdateOnce();
+
+        assertTrue(board.getShot().isVisible());
+        assertFalse(board.getAliens().getFirst().isDying());
     }
     //Camino 1-2-3-4-11-13-14
     @Test
     public void UpdateShots4(){
-        Board board = new Board();
         board.setShot(new Shot(5,5));
         List<Alien> aliens = new ArrayList<>();
         Alien alien = new Alien(10,10);
         alien.die();
         aliens.add(alien);
         board.setAliens(aliens);
-        //board.update();
+
+        callUpdateOnce();
+
+        assertTrue(board.getShot().isVisible());
+        assertFalse(board.getAliens().getFirst().isDying());
     }
     //Camino 1-2-3-4-5-6-11-13-14
     @Test
     public void UpdateShots5() {
-        Board board = new Board();
         board.setShot(new Shot(9, 5));
         List<Alien> aliens = new ArrayList<>();
         Alien alien = new Alien(10, 10);
         aliens.add(alien);
         board.setAliens(aliens);
-        //board.update();
+
+        callUpdateOnce();
+
+        assertTrue(board.getShot().isVisible());
+        assertFalse(board.getAliens().getFirst().isDying());
     }
     //Camino 1-2-3-4-5-6-7-11-13-14
     @Test
     public void UpdateShots6(){
-        Board board = new Board();
         board.setShot(new Shot(11+ Commons.ALIEN_WIDTH,5));
         List<Alien> aliens = new ArrayList<>();
         Alien alien = new Alien(10,10);
@@ -84,36 +109,42 @@ public class BoardUpdateShots {
     //Camino 1-2-3-4-5-6-7-8-11-13-14
     @Test
     public void UpdateShots7(){
-        Board board = new Board();
         board.setShot(new Shot(10 + Commons.ALIEN_WIDTH ,5));
         List<Alien> aliens = new ArrayList<>();
         Alien alien = new Alien(10,10);
         aliens.add(alien);
         board.setAliens(aliens);
-        //board.update();
-    }
+
+        callUpdateOnce();
+
+        assertTrue(board.getShot().isVisible());
+        assertFalse(board.getAliens().getFirst().isDying());    }
     //Camino 1-2-3-4-5-6-7-8-9-11-13-14
     @Test
     public void UpdateShots8(){
-        Board board = new Board();
         board.setShot(new Shot(10 + Commons.ALIEN_WIDTH - 1,11+Commons.ALIEN_HEIGHT));
         List<Alien> aliens = new ArrayList<>();
         Alien alien = new Alien(10,10);
         aliens.add(alien);
         board.setAliens(aliens);
-        //board.update();
-    }
+
+        callUpdateOnce();
+
+        assertTrue(board.getShot().isVisible());
+        assertFalse(board.getAliens().getFirst().isDying());    }
     //Camino 1-2-3-4-5-6-7-8-9-10-11-13-14
     @Test
     public void UpdateShots9(){
-        Board board = new Board();
         board.setShot(new Shot(10 + Commons.ALIEN_WIDTH - 1,10+Commons.ALIEN_HEIGHT));
         List<Alien> aliens = new ArrayList<>();
         Alien alien = new Alien(10,10);
         aliens.add(alien);
         board.setAliens(aliens);
         board.getShot().setY(-10);
-        //board.update();
-    }
+
+        callUpdateOnce();
+
+        assertTrue(board.getShot().isVisible());
+        assertTrue(board.getAliens().getFirst().isDying());    }
 
 }
