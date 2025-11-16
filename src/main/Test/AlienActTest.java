5a3bf4a2a3510c760cfaee91fce9771ce6541307
SergioLopezz20 <sergiolopeez.p@gmail.com>
2025-11-16 13:18:08 +0100

    Caja Blanca Alienact, gameInit,playerInit

---
+++ src/main/Test/AlienActTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import space_invaders.sprites.Alien;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AlienActTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void alienActcorrecto(){
+        Alien alien=new Alien(10,0);
+        alien.act(5);
+        assertEquals(5, alien.getX());
+    }
+}
---
+++ src/main/Test/PlayerInitTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import space_invaders.sprites.Player;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class PlayerInitTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void playerInitCorrecto(){
+       Player player=new Player();
+       assertNotEquals(null,player);
+    }
+}
